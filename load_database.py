8e3371b3ea6b9bfdff299a9d8ed94fb77dc19170
Alberto Ivo Vieira <albertoivo@gmail.com>
2018-05-20 15:26:57 -0300

    adicionando novos registros

--- load_database.py
+++ load_database.py
@@ -8,18 +8,34 @@ Base.metadata.bind = engine
 DBSession = sessionmaker(bind=engine)
 session = DBSession()
 
-cat1 = Category(name='Car')
-session.add(cat1)
+#   category 1   #
+
+cat = Category(name='Car')
+session.add(cat)
 session.commit()
 
-item1 = Item(title='EcoSport', description='SUV', category=cat1)
+item1 = Item(title='EcoSport', description='SUV', category=cat)
 session.add(item1)
 session.commit()
 
-item2 = Item(title='Civic', description='Sedan', category=cat1)
+item2 = Item(title='Civic', description='Sedan', category=cat)
 session.add(item2)
 session.commit()
 
-item3 = Item(title='Focus', description='hatchSUV', category=cat1)
+item3 = Item(title='Focus', description='hatch', category=cat)
 session.add(item3)
+session.commit()
+
+#   category 2   #
+
+cat = Category(name='Movies')
+session.add(cat)
+session.commit()
+
+item1 = Item(title='InterStellar', description='Fiction', category=cat)
+session.add(item1)
+session.commit()
+
+item2 = Item(title='Saving Private Ryan', description='Drama', category=cat)
+session.add(item2)
 session.commit()
