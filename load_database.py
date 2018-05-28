d3b9fb212276d5a2400655dab26d36ee416077e5
Alberto Ivo Vieira <albertoivo@gmail.com>
2018-05-28 15:57:43 -0300

    removendo a config de track modicifications

--- load_database.py
+++ load_database.py
@@ -3,6 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from database_setup import Category, Item, db
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///itemcatalog.db'
 
 
