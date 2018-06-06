84cfa2526028f8089c20872752ee4293a6fae457
Alberto Ivo Vieira <albertoivo@gmail.com>
2018-06-06 15:24:08 -0300

    usuario no item

--- model.py
+++ model.py
@@ -17,6 +17,17 @@ class User(db.Model):
     email = db.Column(db.String(250), nullable=False)
     picture = db.Column(db.String(250))
 
+    @property
+    def serialize(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'email': self.email
+        }
+
+    def __repr__(self):
+        return '<User %r>' % self.name
+
 
 class Category(db.Model):
     __tablename__ = 'category'
@@ -46,6 +57,8 @@ class Item(db.Model):
                         default=datetime.utcnow, onupdate=datetime.utcnow)
     cat_id = db.Column(db.Integer, db.ForeignKey('category.id'))
     category = db.relationship(Category, backref=db.backref("Item", cascade="all,delete"))
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    user = db.relationship(User)
 
     @property
     def serialize(self):
