7ce936fb96c974a3f029e4ee7ecd9b7db3213740
Alberto Ivo Vieira <albertoivo@gmail.com>
2018-12-01 11:54:42 -0200

    ===

--- itemCatalogApp/static/js/utils.js
+++ itemCatalogApp/static/js/utils.js
@@ -1,7 +1,7 @@
 // Used to toggle the menu on small screens when clicking on the menu button
 function toggleMenu() {
-    var x = document.getElementById('bar-mobile')
-    if (x.className.indexOf('show') == -1) {
+  let x = document.getElementById('bar-mobile')
+  if (x.className.indexOf('show') === -1) {
         x.className += ' show'
     } else {
         x.className = x.className.replace(' show', '')
